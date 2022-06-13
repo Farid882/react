6843460953bda6341da2437c81c107abe6fe22d0
Farid882 <56222467+Farid882@users.noreply.github.com>
2022-06-13 16:08:33 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+
+
+function App() {
+
+      <header className="App-header">
+
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+
+          </a>
+
+          <button className="button">Нажми на меня!</button>
+      </header>
+
+
+}
+
+
+export default App;
